6fa1afae2d3c468cf3c4ba4b754671b0cf2d859a
riddle-nazo-story <yumeda0107@gmail.com>
2026-07-08 21:59:30 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBoCDPMuvHmFCW5alpzcUuq2rrEvsWrW4U",
+  authDomain: "riddle-story-member.firebaseapp.com",
+  projectId: "riddle-story-member",
+  storageBucket: "riddle-story-member.firebasestorage.app",
+  messagingSenderId: "383421245640",
+  appId: "1:383421245640:web:6316558557f125826b5dfd",
+
+  // Realtime Databaseを作成したあと、ここにURLを入れる
+  databaseURL: "ここにRealtime DatabaseのURL"
+};
